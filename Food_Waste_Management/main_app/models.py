6f6a8f474d5f7f58cadc4a8509bc468e312c0b19
Harsh Gupta <harshgupta.11dec@gmail.com>
2017-02-19 18:56:02 +0530

    Load IIT Mandi database

--- Food_Waste_Management/main_app/models.py
+++ Food_Waste_Management/main_app/models.py
@@ -9,15 +9,8 @@ class Student(models.Model):
 	name = models.CharField(max_length = 100)
 	branch = models.CharField(max_length = 10)
 	hostel = models.CharField(max_length = 100)
-	roomNum = models.IntegerField()
+	roomNum = models.CharField(max_length = 10)
 	contactNum = models.CharField(max_length = 15)
 
-	def __init__(self,rollNum, name, branch, hostel, roomNum, contactNum):
-		self.rollNum = rollNum
-		self.name = name
-		self.branch = branch
-		self.hostel = hostel
-		self.roomNum = roomNum
-		self.contactNum = contactNum
 	def __str__(self):
 		return self.rollNum
--- Food_Waste_Management/main_app/views.py
+++ Food_Waste_Management/main_app/views.py
@@ -1,5 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect, HttpResponse
+from models import Student
+import requests
+from bs4 import BeautifulSoup
 # Create your views here.
 def index(request):
 	return render(request, 'index.html');
+
+def load(request):
+	#url = 'https://insite.iitmandi.ac.in/directory/students/iphd.php'
+
+	r = requests.get(url)
+
+	soup = BeautifulSoup(r.content,"lxml")
+
+	data = soup.find_all('td');
+	length = len(data)
+	for i in range(3,length,9):
+		rollNum = data[i].text
+		name = data[i+1].text
+		branch = data[i+2].text
+		hostel = data[i+3].text
+		roomNum = data[i+4].text
+		contactNum = data[i+8].text
+		student = Student(rollNum = rollNum,name = name,branch = branch,hostel = hostel,roomNum = roomNum,contactNum = contactNum) 
+		student.save();
+	return HttpResponse("Student loaded successfully")
